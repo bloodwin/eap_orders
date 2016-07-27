6b2c3601676ef5726e97cc799dddffe596bae23e
Anna Bloodwina <dev@bloodwin.ru>
2016-07-27 15:08:24 +0300

    Clear globals in core.php

--- functions/core.php
+++ functions/core.php
@@ -59,84 +59,65 @@ function eap_get_status_id($status_name){
 }
 
 //Отображение свойств заказа
-function eap_order_ID(){
-    global $eap_order;
+function eap_order_ID($eap_order){
     echo $eap_order->order_id;
 }
-function eap_order_date(){
-    global $eap_order;
+function eap_order_date($eap_order){
     echo $eap_order->created;
 }
-function eap_order_fio(){
-    global $eap_order;
+function eap_order_fio($eap_order){
     echo $eap_order->fio;
 }
-function eap_order_address(){
-    global $eap_order;
+function eap_order_address($eap_order){
     echo $eap_order->address;
 }
-function eap_order_email(){
-    global $eap_order;
+function eap_order_email($eap_order){
     echo $eap_order->email;
 }
-function eap_order_phone(){
-    global $eap_order;
+function eap_order_phone($eap_order){
     echo $eap_order->phone;
 }
-function eap_order_user_comment(){
-    global $eap_order;
+function eap_order_user_comment($eap_order){
     echo $eap_order->author_comment;
 }
-function eap_number_products(){
-    global $eap_order;
+function eap_number_products($eap_order){
     echo $eap_order->items_count;
 }
-function eap_order_price(){
-    global $eap_order;
+function eap_order_price($eap_order){
     $price = apply_filters('order_price',$eap_order->order_price,$eap_order);
     echo $price;
 }
-function eap_order_status(){
-    global $eap_order;
+function eap_order_status($eap_order){
     echo eap_get_status_name_order($eap_order->order_status);
 }
-function eap_order_status_date(){
-    global $eap_order;
+function eap_order_status_date($eap_order){
     echo $eap_order->order_status_date;
 }
-function eap_order_basket_full(){
-    global $eap_order;
+function eap_order_basket_full($eap_order){
     echo $eap_order->basket_full;
 }
 
 //Отображение свойств товара
-function eap_product_ID(){
-    global $eap_product;
+function eap_product_ID($eap_product){
     echo $eap_product->product_ID;
 }
-function eap_product_title(){
-    global $eap_product;
+function eap_product_title($eap_product){
     echo $eap_product->product_name;
 }
-function eap_product_price(){
-    global $eap_product;
+function eap_product_price($eap_product){
     echo $eap_product->product_price;
 }
-function eap_product_number(){
-    global $eap_product;
+function eap_product_number($eap_product){
     echo $eap_product->items_count;
 }
-function eap_product_summ(){
-    global $eap_product;
+function eap_product_summ($eap_product){
     echo $eap_product->summ_price;
 }
-function eap_product_permalink(){
-    global $eap_product;
+function eap_product_permalink($eap_product){
     echo $eap_product->permalink;
 }
 //Получаем данные заказа
 function eap_get_order($eap_order_id){
-    global $wpdb,$eap_order,$eap_product;
     $query = "SELECT o.*, g.good_name, g.permalink, b.eap_cost, b.quantity, b.eap_good_id, CONCAT_WS(' - ', g.good_name, b.eap_cost, b.quantity, (b.eap_cost * b.quantity)) as good_data
               FROM ".EAP_PREF."orders as o, 
                    ".EAP_PREF."baskets as b,
@@ -144,14 +125,14 @@ function eap_get_order($eap_order_id){
                    WHERE o.eap_order_id = b.eap_order_id and b.eap_good_id=g.eap_good_id";
     $eap_orderdata = $wpdb->get_results($query);
     if(!$eap_orderdata) return false;
-    return eap_setup_orderdata($eap_orderdata);
+    $eap_order = eap_setup_orderdata($eap_orderdata);
+    return $eap_order;
 }
 
 //Получаем все заказы по указанным параметрам
 function eap_get_orders($args){
     global $wpdb;
     $date = array();
-
     $where = '';
     
     $orders_history = "SELECT o.*, g.good_name, g.permalink, b.eap_cost, b.quantity, b.eap_good_id, CONCAT_WS(' - ', g.good_name, b.eap_cost, b.quantity, (b.eap_cost * b.quantity)) as good_data 
@@ -180,9 +161,7 @@ function eap_get_orders($args){
     }
 
     if($wheres){
-        /*if(isset($args['search'])&&$args['search']) $where = implode(' OR ',$wheres);
-        else */
-            $where = implode(' AND ',$wheres);
+        $where = implode(' AND ',$wheres);
     }
 
     if($where) $orders_history .= " AND ".$where;
@@ -240,7 +219,7 @@ function eap_update_status_order($eap_order_id,$status,$user_id=false){
 }
 
 function eap_get_chart_orders($eap_orders){
-    global $eap_order,$chartData,$chartArgs;
+    global $chartData,$chartArgs;
 
     if(!$eap_orders) return false;
 
@@ -253,20 +232,16 @@ function eap_get_chart_orders($eap_orders){
         )
     );
 
-    foreach($eap_orders as $eap_order){
-        //rcl_setup_orderdata($order);
-        //rcl_setup_chartdata($eap_order->order_date,$eap_order->order_price);
+    foreach($eap_orders as $order){
+        $eap_order = eap_get_order($order->id);
         rcl_setup_chartdata($eap_order->created,$eap_order->order_status_date,$eap_order->order_price);
     }
 
     return rcl_get_chart($chartArgs);
 }
 
-
 //Формирование массива данных заказа
 function eap_setup_orderdata($orderdata){
-    global $eap_order,$eap_product;
-
 /*
         o.order_status as order_status,
         o.status_date as status_date,
@@ -310,11 +285,11 @@ function eap_setup_orderdata($orderdata){
         'amount'=>0,
         'products'=>array()
     );
-
+ 
     $count = 0;
 
     foreach($orderdata as $data){ 
-        eap_setup_productdata($data);
+        $eap_product = eap_setup_productdata($data);
         
         if(!$eap_order->order_id) $eap_order->order_id = $data->eap_order_id;
         if(!$eap_order->order_author) $eap_order->order_author = $data->user_id;
@@ -347,7 +322,6 @@ function eap_setup_orderdata($orderdata){
     return $eap_order;
 }
 function eap_setup_productdata($productdata){
-    global $eap_product;
 
     $eap_product = (object)array(
         'product_id'=>0,
@@ -362,17 +336,16 @@ function eap_setup_productdata($productdata){
         'permalink'=>false
     );
     
-        $eap_product->product_id = $productdata->eap_good_id;
-        $eap_product->product_price = $productdata->eap_cost;
-        $eap_product->summ_price = $productdata->eap_cost*$productdata->quantity;
-        $eap_product->items_count = $productdata->quantity;
-        $eap_product->user_id = $productdata->user_id;
-        $eap_product->order_id = $productdata->eap_order_id;
-        $eap_product->created = $productdata->created;
-        $eap_product->order_status = $productdata->order_status;
-        $eap_product->product_name = $productdata->good_name;
-        $eap_product->permalink = $productdata->permalink;
-
+    $eap_product->product_id = $productdata->eap_good_id;
+    $eap_product->product_price = $productdata->eap_cost;
+    $eap_product->summ_price = $productdata->eap_cost*$productdata->quantity;
+    $eap_product->items_count = $productdata->quantity;
+    $eap_product->user_id = $productdata->user_id;
+    $eap_product->order_id = $productdata->eap_order_id;
+    $eap_product->created = $productdata->created;
+    $eap_product->order_status = $productdata->order_status;
+    $eap_product->product_name = $productdata->good_name;
+    $eap_product->permalink = $productdata->permalink;
 
     return $eap_product;
 }
