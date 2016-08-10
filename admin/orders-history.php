71eff50c891386b18141c367c70fc9b11b02eb7a
bloodwin <anna@anna-desktop>
2016-08-10 13:01:23 +0300

    Admin interface

--- admin/orders-history.php
+++ admin/orders-history.php
@@ -8,7 +8,6 @@ add_action('admin_init',array('Eap_Orders_History_Table','update_status_order'))
 
 
 function eap_orders_page_options() {
-    global $Eap_Order_Global;
     $option = 'per_page';
     $args = array(
         'label' => __( 'Orders', 'wp-recall' ),
@@ -16,11 +15,10 @@ function eap_orders_page_options() {
         'option' => 'eap_orders_per_page'
     );
     add_screen_option( $option, $args );
-    $Eap_Orders_Global = new Eap_Orders_History_Table();
 }
 
 function eap_admin_orders_page(){
-    global $Eap_Orders_Global;
+    $Eap_Orders_Global = new Eap_Orders_History_Table();
     $Eap_Orders_Global->prepare_items();
 
     echo '</pre><div class="wrap"><h2>' . __('Orders history', 'wp-recall') . '</h2>';
